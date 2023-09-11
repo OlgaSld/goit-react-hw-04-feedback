06305a06430ab4662e4379fb684be1c067089afc
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-09-11 22:43:32 +0300

    fixed options

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,25 +1,22 @@
-import { FeedbackContainer, Button } from "./FeedbackOptions_styled";
+import { FeedbackContainer, Button } from './FeedbackOptions_styled';
 import PropTypes from 'prop-types';
 
-
 export const Feedback = ({ options, onLeaveFeedback }) => {
-  const optionsArr = Object.keys(options);
-    return (
-      <FeedbackContainer>
-        {optionsArr.map(option => (
-          <Button
-            key={option}
-            type="button"
-            name={option}
-            onClick={onLeaveFeedback}
-          >
-            {option}
-          </Button>
-        ))}
-      </FeedbackContainer>
-    );
-}
-
+  return (
+    <FeedbackContainer>
+      {options.map(option => (
+        <Button
+          key={option}
+          type="button"
+          name={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </Button>
+      ))}
+    </FeedbackContainer>
+  );
+};
 
 Feedback.propTypes = {
   option: PropTypes.arrayOf(PropTypes.string),
